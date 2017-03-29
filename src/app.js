e72b0851ef75ddf15d25132aef9dd3068352acb5
Frank Reding <frank.reding@gmail.com>
2017-03-29 16:56:32 +0200

    Init

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+new Vue({
+	el: '#app',
+
+	components: {
+		Card
+	}
+});
---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="card">
+		<h1>Hello, world!</h1>
+	</div>
+</template>
+
+<script>
+import vSelect from 'vue-select';
+
+export default {
+	components: {
+		vSelect
+	}
+};
+</script>
