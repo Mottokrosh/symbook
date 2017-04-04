c609d9e302c27a17fa7e2c33c01b33a928690f24
Frank Reding <frank.reding@gmail.com>
2017-04-04 13:44:17 +0200

    Tiniest styling

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/')
+mix.setPublicPath('dist/')
+   .js('src/app.js', 'dist/')
    .sass('src/app.scss', 'dist/')
    .copy('src/data.json', 'dist/')
    .copy('src/index.html', 'dist/');
